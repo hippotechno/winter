3a005889692d8ccff00ba93e0e3d60372b0ceaef
Tu Le <khuta441@gmail.com>
2026-04-20 14:01:08 +0700

    [update] bổ sung file config ở cấp hệ thống cho plugin Hippo.Core

---
+++ config/hippo/core/config.example.php
@@ -0,0 +1,72 @@
+<?php
+
+return [
+    'enable_multisite' => true,
+    'sites' => [
+        [
+            'host' => '6am.tumlumtala.test',
+            'theme' => 'six-am-beginner',
+            'skin' => 'uikit'
+        ],
+        [
+            'host' => 'demo.tumlumtala.test',
+            'theme' => 'demo',
+            'skin' => 'uikit'
+        ],
+        [
+            'host' => 'bg360.tumlumtala.test',
+            'theme' => 'wn-bg360-theme',
+            'skin' => 'uikit'
+        ],
+        [
+            'host' => 'decorgreen.tumlumtala.test',
+            'theme' => 'wn-decorgreen-theme',
+            'skin' => 'uikit'
+        ],
+        [
+            'host' => 'rise.tumlumtala.test',
+            'theme' => 'wn-rise-theme',
+            'skin' => 'uikit'
+        ],
+        [
+            'host' => 'linhaisushi.tumlumtala.test',
+            'theme' => 'wn-linhaisushi-theme',
+            'skin' => 'uikit'
+        ],
+        [
+            'host' => 'thungiay.tumlumtala.test',
+            'theme' => 'wn-thungiay-theme',
+            'skin' => 'uikit'
+        ],
+        [
+            'host' => 'catwine.tumlumtala.test',
+            'theme' => 'wn-catwine-theme',
+            'skin' => 'uikit'
+        ],
+        [
+            'host' => 'thomo.tumlumtala.test',
+            'theme' => 'wn-thomo-theme',
+            'skin' => 'uikit'
+        ],
+        [
+            'host' => 'gaion.tumlumtala.test',
+            'theme' => 'wn-gaion-theme',
+            'skin' => 'uikit'
+        ],
+        [
+            'host' => 'tombo.tumlumtala.test',
+            'theme' => 'wn-tombo-theme',
+            'skin' => 'uikit'
+        ],
+        [
+            'host' => 'tomboooo.tumlumtala.test',
+            'theme' => 'wn-tombo-theme',
+            'skin' => 'uikit'
+        ],
+        [
+            'host' => 'tltl.tumlumtala.test',
+            'theme' => 'wn-tltl-theme',
+            'skin' => 'uikit'
+        ],
+    ]
+];
