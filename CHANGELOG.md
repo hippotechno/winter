9690b6191df16093b762c966a5a94d8268492f09
Tu Le <khuta441@gmail.com>
2026-05-03 11:20:55 +0700

    [update] ver 1.0.7

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,12 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [1.0.7]
+
+### Changed
+
+- Update production multisite domain-to-theme mapping in `config/hippo/core/config.example.php`.
+
 ## [1.0.6]
 
 ### Added
--- config/hippo/core/config.example.php
+++ config/hippo/core/config.example.php
@@ -4,99 +4,54 @@ return [
     'enable_multisite' => true,
     'sites' => [
         [
-            'host' => 'www.tuimuon.xyz',
-            'theme' => 'hippo-default-theme',
-            'skin' => 'uikit'
-        ],
-        [
-            'host' => 'silverarrowsoftware.com',
-            'theme' => 'sas-sas-21'
-        ],
-        [
-            'host' => 'muitenbac.com',
-            'theme' => 'sas-sas-21'
-        ],
-        [
-            'host' => 'tulutala.com',
-            'theme' => 'hippo-default-theme',
-            'skin' => 'uikit'
-        ],
-        [
-            'host' => 'decorgreensaigon.com',
-            'theme' => 'wn-decorgreen-theme'
-        ],
-        [
-            'host' => 'trammb.com',
-            'theme' => 'hippo-default-theme'
-        ],
-        [
-            'host' => 'boardgame360.com',
-            'theme' => 'wn-bg360-theme',
-            'skin' => 'uikit'
-        ],
-        // [
-        //      'host' => 'bumen.vn',
-        //      'theme' => 'sas-bumen-17'
-        // ],
-        [
-            'host' => 'phanbokho.com',
-            'theme' => 'hippo-datsinh-18'
-        ],
-        [
-            'host' => 'thunghero.com',
-            'theme' => 'wn-thungiay-theme'
-        ],
-        [
-            'host' => 'thungjet.com',
-            'theme' => 'wn-thungiay-theme'
-        ],
-        [
-            'host' => 'demo.tulutala.com',
-            'theme' => 'oc-theme-bumen-17'
-        ],
-        [
-            'host' => 'catwine.tulutala.com',
-            'theme' => 'wn-catwine-theme'
-        ],
-        [
-            'host' => 'rise.tulutala.com',
-            'theme' => 'wn-rise-theme'
-        ],
-        [
-            'host' => 'boardgame360.com',
-            'theme' => 'wn-bg360-theme'
-        ],
-        [
-            'host' => 'gaion.dev',
-            'theme' => 'wn-gaion-theme'
-        ],
-        [
             'host' => 'vn.sbitdev.kr',
-            'theme' => 'wn-gaion-theme'
+            'theme' => 'wn-gaion-theme',
+        ],
+        [
+            'host' => 'www.muitenbac.com',
+            'theme' => 'wn-sas-theme',
+        ],
+        [
+            'host' => 'www.tulutala.com',
+            'theme' => 'hippo-default-theme',
+            'skin' => 'uikit',
+        ],
+        [
+            'host' => 'www.boardgame360.com',
+            'theme' => 'wn-bg360-theme',
+            'skin' => 'uikit',
+        ],
+        [
+            'host' => 'www.decorgreensaigon.com',
+            'theme' => 'wn-decorgreen-theme',
+        ],
+        [
+            'host' => 'www.myquy.com',
+            'theme' => 'wn-thomo-theme',
+        ],
+        [
+            'host' => 'www.cuakhauthomo.com',
+            'theme' => 'wn-thomo-theme',
+        ],
+        [
+            'host' => 'www.tombocaffe.com',
+            'theme' => 'wn-tombo-theme',
+        ],
+        [
+            'host' => 'www.tomboramen.com',
+            'theme' => 'wn-tombo-theme',
         ],
         [
             'host' => 'bumen.tulutala.com',
-            'theme' => 'oc-theme-bumen-17'
+            'theme' => 'oc-theme-bumen-17',
         ],
         [
-            'host' => 'tombocaffe.com',
-            'theme' => 'wn-tombo-theme'
+            'host' => 'www.thunghero.com',
+            'theme' => 'wn-thungiay-theme',
         ],
         [
-            'host' => 'tomboramen.com',
-            'theme' => 'wn-tombo-theme'
-        ],
-        [
-            'host' => 'cuakhaumyquytay.com',
-            'theme' => 'wn-thomo-theme'
-        ],
-        [
-            'host' => 'cuakhauthomo.com',
-            'theme' => 'wn-thomo-theme'
-        ],
-        [
-            'host' => 'myquy.com',
-            'theme' => 'wn-thomo-theme'
+            'host' => 'www.thungjet.com',
+            'theme' => 'wn-thungiay-theme',
         ],
     ]
 ];
